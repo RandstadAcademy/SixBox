1390748de8c6f6de6d2c81344c7ec3f1b1b2ced6
Aula Randstad <Aula Randstad@PC2225>
2017-02-10 15:42:28 +0100

    Add project files.

---
+++ SixPanel/Controllers/DashBoardController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SixPanel.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        // GET: DashBoard
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult One()
+        {
+            return View();
+        }
+
+        public ActionResult Two()
+        {
+            return View();
+        }
+
+        public ActionResult Three()
+        {
+            return View();
+        }
+
+    }
+}
